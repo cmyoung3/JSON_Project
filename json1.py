6036f2d2c00cc0c077da798acb6fc4ead11f7f41
cmyoung3 <Caleb_Young1@baylor.edu>
2021-10-06 16:34:29 -0500

    classwork 10/6

--- json1.py
+++ json1.py
@@ -1,6 +1,6 @@
 import json
 
-infile = open('eq_data_1_day_m1.json', 'r')
+infile = open('eq_data_30_day_m1.json', 'r')
 outfile = open('readable_eq_data.json', 'w')
 
 eqdata = json.load(infile)
@@ -15,19 +15,28 @@ list_of_eqs = eqdata["features"]
 
 mags = []
 lats = []
-longs = []
+lons = []
 
 for eq in list_of_eqs:
     mag = eq["properties"]["mag"]
-    lat = eq["geometry"]["coordinates"][0]
-    long = eq["geometry"]["coordinates"][1]
+    lat = eq["geometry"]["coordinates"][1]
+    lon = eq["geometry"]["coordinates"][0]
     mags.append(mag)
     lats.append(lat)
-    longs.append(long)
+    lons.append(lon)
 
-print(mags)
-print(lats)
-print(longs)
+print(mags[:5])
+print(lats[:5])
+print(lons[:5])
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title="Global Earthquakes 30 Day")
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='globalearthquakes30day.html')
+
 '''
 for magnitude in eqdata():
     print(eqdata["features"]["mag"])
---
+++ json2.py
@@ -0,0 +1,57 @@
+import json
+
+infile = open('eq_data_30_day_m1.json', 'r')
+outfile = open('readable_eq_data.json', 'w')
+
+eqdata = json.load(infile)
+
+json.dump(eqdata,outfile,indent=4)
+
+print(eqdata["metadata"]["count"])
+
+print(len(eqdata["features"]))
+
+list_of_eqs = eqdata["features"]
+
+mags, lats, lons, hover_texts = [],[],[],[]
+
+for eq in list_of_eqs:
+    mag = eq["properties"]["mag"]
+    lat = eq["geometry"]["coordinates"][1]
+    lon = eq["geometry"]["coordinates"][0]
+    title = eq['properties']['title']
+    mags.append(mag)
+    lats.append(lat)
+    lons.append(lon)
+    hover_texts.append(title)
+
+print(mags[:5])
+print(lats[:5])
+print(lons[:5])
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+#data = [Scattergeo(lon=lons, lat=lats)]
+
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat':lats,
+    'text': hover_texts,
+    'marker':{
+        'size':[5*m for m in mags],
+        'color':mags, 
+        'colorscale': 'Viridis',
+        'reversescale':True,
+        'colorbar':{'title':'Magnitude'}
+    }
+}]
+my_layout = Layout(title="Global Earthquakes 30 Day")
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='globalearthquakes30day.html')
+
+'''
+for magnitude in eqdata():
+    print(eqdata["features"]["mag"])
+'''

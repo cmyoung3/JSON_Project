b562a1c333e91b94294961a046f110a915c52e53
cmyoung3 <Caleb_Young1@baylor.edu>
2021-10-10 20:11:43 -0500

    saved homework 10/10

---
+++ HW_JSON_9_14.py
@@ -0,0 +1,36 @@
+import json
+
+infile = open('US_fires_9_14.json', 'r')
+
+FireData = json.load(infile)
+
+brights, lats, lons = [],[],[]
+
+for fires in FireData:
+    bright = fires["brightness"]
+    lat = fires["latitude"]
+    lon = fires["longitude"]
+    if bright >= 450:
+        brights.append(bright)
+        lats.append(lat)
+        lons.append(lon)
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat':lats,
+    'marker':{
+        'color': brights,
+        'colorscale': 'Viridis',
+        'reversescale':True,
+        'colorbar':{'title':'Brightness'}
+    }
+}]
+my_layout = Layout(title="US Wildfires Sept 14-30")
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='usfires9_14.html')
+    
+
